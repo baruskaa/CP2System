65a8ca637426dab41009314a7c5ab915ca59839e
kyshgyl <kyshatoribio@gmail.com>
2026-02-25 17:50:48 +0800

    Kysha

---
+++ src/kysha.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kyshgel
+ */
+public class kysha {
+    public static void main(String[] args) {
+    System.out.print("Kysha Test");
+    }
+}
